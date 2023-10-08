f2e69dbe45fe8cf321ee49a9d04f5c3b90816898
StutiMittal <stutimittal111@gmail.com>
2023-10-08 21:39:56 +0530

    google font package in yaml

--- lib/pages/logininpage.dart
+++ lib/pages/logininpage.dart
@@ -8,6 +8,13 @@ class LoginPage extends StatelessWidget {
         child: Column(
           children: [
             Image.asset("assets/images/login_img.png", fit: BoxFit.cover),
+            Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            )
           ],
         ));
   }
