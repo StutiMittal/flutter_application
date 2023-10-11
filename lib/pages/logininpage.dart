fd4ca69bef98e56ec7e1d56efd66a8da3b105293
StutiMittal <stutimittal111@gmail.com>
2023-10-11 18:04:11 +0530

    next page with delay

--- lib/pages/logininpage.dart
+++ lib/pages/logininpage.dart
@@ -56,11 +56,12 @@ class _LoginPageState extends State<LoginPage> {
                         height: 40.0,
                       ),
                       InkWell(
-                        onTap: () {
+                        onTap: () async {
                           setState(() {
                             chngbutton = true;
                           });
-                          //Navigator.pushNamed(context, MyRoutes.homeRoute);
+                          await Future.delayed(Duration(seconds: 1));
+                          Navigator.pushNamed(context, MyRoutes.homeRoute);
                         },
                         child: AnimatedContainer(
                           duration: Duration(seconds: 1),
