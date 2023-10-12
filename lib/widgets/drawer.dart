af2fb9dc127bf42877f4f45c19caf14fe3d699ca
StutiMittal <stutimittal111@gmail.com>
2023-10-12 10:01:09 +0530

    basic header

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
 
+class Mydrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          DrawerHeader(
+              child: UserAccountsDrawerHeader(
+            margin: EdgeInsets.zero,
+            accountName: Text("Stuti"),
+            accountEmail: Text("stuti111@gmail.com"),
+          ))
+        ],
+      ),
+    );
+  }
+}
