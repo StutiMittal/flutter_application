1e249a19d2f9a45335565b059b9a89759812135b
StutiMittal <stutimittal111@gmail.com>
2023-10-12 12:07:40 +0530

    theme changes made important

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,1 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.lato().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        //textTheme: Theme.of(context).textTheme,
+        //     titleTextStyle: Theme.of(context).textTheme.headline6, // Use titleTextStyle
+        //  toolbarTextStyle: Theme.of(context).textTheme.,
+        titleTextStyle: TextStyle(color: Colors.black),
+      ));
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
+      );
+}
