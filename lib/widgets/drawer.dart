b922a8ebea616ceda71d02803fc1a3f6153f4f83
StutiMittal <stutimittal111@gmail.com>
2023-10-12 10:36:53 +0530

    drawer changes and feature

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -3,16 +3,28 @@ import 'package:flutter/material.dart';
 class Mydrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final imageUrl =
+        "https://media.istockphoto.com/id/1413848883/photo/common-marigold-flowerhead-isolated-on-white-background.jpg?s=1024x1024&w=is&k=20&c=Ve1ERE4Tkqa17KPW5Y4gP61S-V1aT5aFt5javb2bRBs=";
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-              child: UserAccountsDrawerHeader(
-            margin: EdgeInsets.zero,
-            accountName: Text("Stuti"),
-            accountEmail: Text("stuti111@gmail.com"),
-          ))
-        ],
+      child: Container(
+        color: Colors.deepPurple,
+        child: ListView(
+          //padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                  margin: EdgeInsets.zero,
+                  // decoration:
+                  //     BoxDecoration(color: Color.fromARGB(255, 223, 82, 21)),
+                  accountName: Text("Stuti"),
+                  accountEmail: Text("stuti111@gmail.com"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: NetworkImage(imageUrl),
+                  ),
+                ))
+          ],
+        ),
       ),
     );
   }
