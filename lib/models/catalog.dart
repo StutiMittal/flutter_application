c3cc89a3b4ec0df2cd76bb95cb9a83ff46f7a554
StutiMittal <stutimittal111@gmail.com>
2023-10-13 22:50:40 +0530

    datatype of id changed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,5 @@
 class Item {
-  final String? id;
+  final int? id;
   final String? name;
   final String? desc;
   final num? price;
@@ -11,7 +11,7 @@ class Item {
 
 final products = [
   Item(
-      id: "Codepur001",
+      id: 1,
       name: "iPhone 12 Pro",
       desc: "Apple iPhone 12th generation",
       price: 999,
